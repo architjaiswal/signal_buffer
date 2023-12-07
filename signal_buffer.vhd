89f54e167ebcb0eb4d160f1eee1f0824554cccde
architjaiswal <archit.jaiswal@ufl.edu>
2023-12-07 16:51:16 -0500

    used code from delay.vhd and made it look similar instead of instantiating it in the design

--- signal_buffer.vhd
+++ signal_buffer.vhd
@@ -18,90 +18,6 @@
 
 ----------------------------------------------------------------------------------
 
------------------------ COUNTER ENTITY ------------------------------------------
-library IEEE;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use ieee.math_real.all;
-
-entity counter is
-    generic (MAX_COUNT_NUMBER: integer := 127); -- it will count from 0 to 127, so total 128 times
-    port (
-        clk : in std_logic;
-        rst : in std_logic;
-        add_one : in std_logic;
-        subtract_one : in std_logic;
-        -- output : out std_logic_vector(integer(ceil(log2(real(MAX_COUNT_NUMBER+1))))-1 downto 0)
-        output : out std_logic -- single bit output (will be 1 when the count reaches MAX_COUNT_NUMBER
-    );
-end counter;
-
-architecture BHV of counter is
-
-    constant NUM_BITS : positive := integer(ceil(log2(real(MAX_COUNT_NUMBER+1))));
-    signal count_r : unsigned(NUM_BITS-1 downto 0);
-
-begin
-
-    process(clk, rst)
-    begin
-
-        if (rst = '1') then
-            count_r <= (others => '0');
-        
-        elsif (rising_edge(clk)) then
-            if (add_one = '1') then
-                if (count_r = MAX_COUNT_NUMBER) then
-                    count_r <= to_unsigned(0, NUM_BITS);
-                else
-                    count_r <= count_r + 1;
-                end if;
-            end if;
-
-            if (subtract_one = '1') then
-                if (count_r /= 0) then
-                    count_r <= count_r - 1;
-                end if;
-            end if;
-        end if;
-
-    end process;
-
-    -- output <= std_logic_vector(count_r); -- uncomment when counter needs to output the number
-    output <= '1' when count_r = MAX_COUNT_NUMBER else '0';
-
-end BHV;
-
------------------------ REGISTER ENTITY ------------------------------------------
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
-entity reg is
-    generic (WIDTH: integer := 16); -- Number of bits in the register
-    port (
-        clk : in std_logic;
-        rst : in std_logic;
-        en : in std_logic;
-        input : in std_logic_vector(WIDTH-1 downto 0);
-        output : out std_logic_vector(WIDTH-1 downto 0)
-        );
-end reg;
-
-architecture BHV of reg is 
-begin
-    process (clk, rst)
-    begin
-        if (rst = '1') then
-            output <= (others => '0');
-        elsif (rising_edge(clk)) then
-            if (en = '1') then
-                output <= input;
-            end if;
-        end if;
-    end process;
-end BHV;
-----------------------------------------------------------------------------------
-
 
 ---------------------- SIGNAL BUFFER Entiry --------------------------------------
 library IEEE;
@@ -109,15 +25,16 @@ use IEEE.STD_LOGIC_1164.ALL;
 -- use work.user_pkg.all; -- UNCOMMENT IT LATER
 
 entity signal_buffer is
-    generic (NUM_ELEMENTS: integer := 128; -- Number of elements in the buffer
-             WIDTH:        integer := 16); -- Number of bits in each element
+    generic (NUM_ELEMENTS: integer          := 128; -- Number of elements in the buffer
+             WIDTH:        integer          := 16;
+             RESET_VALUE:  std_logic_vector := ""); -- Number of bits in each element
     port ( 
         clk : in std_logic;
         rst : in std_logic;
         rd_en  : in std_logic;  -- Just to get acknowledgement from the reader that it has read the current content of the buffer and now is the time to move on and load in the new element
         wr_en  : in std_logic;  -- Control for writing the data into this buffer
-        wr_data : in std_logic_vector(WIDTH-1 downto 0);
-        rd_data : out std_logic_vector(NUM_ELEMENTS*WIDTH-1 downto 0);
+        wr_data : in std_logic_vector(WIDTH-1 downto 0); -- INPUT DATA | coming from the FIFO
+        rd_data : out std_logic_vector(NUM_ELEMENTS*WIDTH-1 downto 0); -- OUTPUT DATA | going to the pipeline/datapath to compute the 1D convolution
         empty : out std_logic;
         full : out std_logic
     );
@@ -125,35 +42,78 @@ end signal_buffer;
 
 architecture Behavioral of signal_buffer is
 
-    -- Defining an array of elements -- COMMENT ALL OF THESE LATER BECAUSE ALREADY PRESENT IN user_pkg.vhd
-    -- constant C_KERNEL_SIZE           : positive := 128; 
-    -- constant C_SIGNAL_WIDTH          : positive := 16;
-    -- subtype SIGNAL_WIDTH_RANGE is natural range C_SIGNAL_WIDTH-1 downto 0;
-    -- type window is array(0 to C_KERNEL_SIZE) of std_logic_vector(SIGNAL_WIDTH_RANGE);
-    
-    subtype SIGNAL_WIDTH_RANGE is natural range WIDTH-1 downto 0;
-    type window is array(0 to NUM_ELEMENTS) of std_logic_vector(SIGNAL_WIDTH_RANGE);
+    -- Instantiate a counter
+    component counter
+        generic (MAX_COUNT_NUMBER: integer); -- it will count from 0 to 127, so total 128 times
+        port (
+            clk : in std_logic;
+            rst : in std_logic;
+            add_one : in std_logic;
+            subtract_one : in std_logic;
+            output : out std_logic -- single bit output (will be 1 when the count reaches MAX_COUNT_NUMBER)
+        );
+    end component;
 
-    -- Creating an instance of array
-    signal data_array : window;
     signal has_complete_window : std_logic;
-    signal pipeline_in : std_logic_vector(rd_data'range);
 
 begin
 
-    U_REG : for i in 0 to NUM_ELEMENTS-1 generate
-        U_REG : entity work.reg
-            generic map(WIDTH => WIDTH)
-            port map(
-                clk => clk,
-                rst => rst,
-                en  => wr_en,
-                input  => data_array(i),
-                output => data_array(i+1)
-            );
-    end generate;
+    -- handle a special case of having buffer of element size zero
+    U_NUM_ELEMENT_EQ_0: if NUM_ELEMENTS = 0 generate
+        rd_data <= wr_data; 
+    end generate U_NUM_ELEMENT_EQ_0;
 
-    U_DATAPATH_COUNTER : entity work.counter
+    U_NUM_ELEMENTS_GT_0: if NUM_ELEMENTS > 0 generate
+        -- Defining an array of elements 
+        subtype SIGNAL_WIDTH_RANGE is natural range WIDTH-1 downto 0;
+        type window is array(0 to NUM_ELEMENTS) of std_logic_vector(SIGNAL_WIDTH_RANGE);
+        
+        -- Creating an instance of array
+        signal data_array : window;
+        signal vectorized_output : std_logic_vector(rd_data'range); -- A vector of all bits of all elements (NUM_ELEMENTS * WIDTH = A long bit vector)
+
+    begin
+
+        process (clk, rst)
+        begin
+            if (rst = '1') then
+                -- Check if the user wants to set some fixed value upon resetting it, otherwise set it to the default (others <= '0')
+                if (RESET_VALUE /= "") then
+                    for i in 0 to NUM_ELEMENTS-1 loop
+                        data_array(i) <= RESET_VALUE;
+                    end loop;
+                else
+                    for i in 0 to NUM_ELEMENTS-1 loop
+                        data_array(i) <= (others => '0');
+                    end loop;
+                end if;
+
+            elsif (rising_edge(clk)) then
+                if (wr_en = '1') then
+                    data_array(0) <= wr_data;
+                end if;
+
+                if (NUM_ELEMENTS > 1) then
+                    for i in 0 to NUM_ELEMENTS-2 loop
+                        if (wr_en = '1') then
+                            data_array(i+1) <= data_array(i);
+                        end if;
+                    end loop;
+                end if;
+            end if;
+        end process;
+
+        -- put pipeline inputs into a big vector (i.e., "vectorize")
+        U_VECTORIZE : for i in 0 to NUM_ELEMENTS-1 generate
+        vectorized_output((i+1)*WIDTH-1 downto i*WIDTH) <= data_array(i);
+        end generate;
+
+        rd_data <= vectorized_output;
+
+    end generate U_NUM_ELEMENTS_GT_0;
+
+    
+    U_DATAPATH_COUNTER : counter
     generic map(MAX_COUNT_NUMBER => NUM_ELEMENTS)
     port map(
         clk => clk,
@@ -164,19 +124,7 @@ begin
     );
 
     empty <= not has_complete_window;
-    
-    -- full  <=     has_complete_window; -- This is the normal way of implementing it. Correct, but slower then the following logic
-
-    full  <= has_complete_window and not rd_en; -- trying to increase the throughtput as suggested in signal buffer example
-
-
-    -- put pipeline inputs into a big vector (i.e., "vectorize")
-    U_VECTORIZE : for i in 0 to NUM_ELEMENTS-1 generate
-    pipeline_in((i+1)*WIDTH-1 downto i*WIDTH) <= data_array(i);
-    end generate;
-
-    data_array(0) <= wr_data when wr_en = '1' else data_array(0);
-    
-    rd_data <= pipeline_in;
+    full  <= has_complete_window and not rd_en; -- if rd_en is asserted then the data will be read right away and the buffer will be not full. 
+    -- full <= has_complete_windows; This will only give half of the bandwidth because when the buffer is full, the FIFO will stop and continue until it is not full. This will keep oscilating and will be slower.  
 
 end Behavioral;
